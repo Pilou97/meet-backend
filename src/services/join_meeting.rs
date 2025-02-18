aabf99729146a49d175ada1bc955f7c67fbbcb59
Pierre-Louis <pierrelouis.dubois@tutanota.com>
2025-02-18 17:55:12 +0100

    feat(meeting): create a token

--- src/services/join_meeting.rs
+++ src/services/join_meeting.rs
@@ -46,16 +46,18 @@ pub async fn join_meeting(
 
 #[cfg(test)]
 mod tests {
-    use chrono::Utc;
-    use mockall::predicate::eq;
-
+    use super::JoinMeetingError;
     use crate::{
-        domain::meeting::MeetingId,
+        domain::{
+            meeting::{Meeting, MeetingId, MeetingName},
+            room::RoomToken,
+            studio::StudioId,
+        },
         ports::output::{meeting_repository::MockMeetingRepository, room_manager::MockRoomManager},
         services::join_meeting,
     };
-
-    use super::JoinMeetingError;
+    use chrono::{Days, Utc};
+    use mockall::predicate::eq;
 
     #[tokio::test]
     async fn test_not_found() {
@@ -74,6 +76,71 @@ mod tests {
             join_meeting(&mock_repo, &mock_room_manager, meeting_id, Utc::now()).await
         else {
             panic!("The meeting should not be found");
+        };
+    }
+
+    #[tokio::test]
+    async fn test_too_late() {
+        let meeting_id = MeetingId::new();
+
+        let mut mock_repo = MockMeetingRepository::new();
+        let mock_room_manager = MockRoomManager::new();
+
+        mock_repo
+            .expect_find_meeting()
+            .once()
+            .with(eq(meeting_id.clone()))
+            .return_once(move |meeting_id| {
+                let meeting_id = meeting_id.clone();
+                Box::pin(async move {
+                    Ok(Some(Meeting {
+                        id: meeting_id,
+                        studio_id: StudioId::from(uuid::Uuid::new_v4()),
+                        name: MeetingName::try_from("An old meeting".to_string()).unwrap(),
+                        date: Utc::now().checked_sub_days(Days::new(2)).unwrap(),
+                    }))
+                })
+            });
+
+        let Err(JoinMeetingError::TooLate) =
+            join_meeting(&mock_repo, &mock_room_manager, meeting_id, Utc::now()).await
+        else {
+            panic!("The meeting should exist but in the past");
+        };
+    }
+
+    #[tokio::test]
+    async fn test_ok() {
+        let meeting_id = MeetingId::new();
+
+        let mut mock_repo = MockMeetingRepository::new();
+        let mut mock_room_manager = MockRoomManager::new();
+
+        mock_repo
+            .expect_find_meeting()
+            .once()
+            .with(eq(meeting_id.clone()))
+            .return_once(move |meeting_id| {
+                let meeting_id = meeting_id.clone();
+                Box::pin(async move {
+                    Ok(Some(Meeting {
+                        id: meeting_id,
+                        studio_id: StudioId::from(uuid::Uuid::new_v4()),
+                        name: MeetingName::try_from("Hello there!".to_string()).unwrap(),
+                        date: Utc::now().checked_add_days(Days::new(2)).unwrap(),
+                    }))
+                })
+            });
+
+        mock_room_manager
+            .expect_create_token()
+            .once()
+            .with(eq(meeting_id.clone()))
+            .return_once(|_| Box::pin(async { Ok(RoomToken::from("mytoken".to_string())) }));
+
+        let Ok(_) = join_meeting(&mock_repo, &mock_room_manager, meeting_id, Utc::now()).await
+        else {
+            panic!("A token should be returned");
         };
     }
 }
