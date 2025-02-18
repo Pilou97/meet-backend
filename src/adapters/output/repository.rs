cc35f116c74cd4fccc4daad9f0af1faa40b6a3e7
Pierre-Louis <pierrelouis.dubois@tutanota.com>
2025-02-18 10:08:27 +0100

    feat(repo): execute the migration script

--- src/adapters/output/repository.rs
+++ src/adapters/output/repository.rs
@@ -1,4 +1,5 @@
-use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
+use anyhow::Context;
+use sqlx::{migrate, postgres::PgPoolOptions, Pool, Postgres};
 
 #[derive(Clone)]
 pub struct Repository {
@@ -6,11 +7,17 @@ pub struct Repository {
 }
 
 impl Repository {
-    pub async fn new(uri: &url::Url) -> Result<Repository, sqlx::Error> {
+    pub async fn new(uri: &url::Url) -> Result<Repository, anyhow::Error> {
         let pg_pool = PgPoolOptions::new()
             .max_connections(8)
             .connect(uri.as_str())
-            .await?;
+            .await
+            .context("Cannot connect to postgresql database")?;
+
+        migrate!("./migrations")
+            .run(&pg_pool)
+            .await
+            .context("The database migration failed")?;
 
         Ok(Repository { pg_pool })
     }
